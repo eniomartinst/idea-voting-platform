4e58a3fe0c841630bf90be4656e26b20270c8111
Enio Martins <eniomt1.0@gmail.com>
2026-09-18 11:51:55 -0300

    feat(frontend): cria o componente de login e respetiva interface visual

---
+++ web-app/src/app/features/auth/login/login.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class Login {
+
+}
