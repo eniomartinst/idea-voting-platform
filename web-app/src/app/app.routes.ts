129937c9e25c4b912d166373c526f6d632b06317
Enio Martins <eniomt1.0@gmail.com>
2026-09-18 11:51:55 -0300

    feat(frontend): configura as rotas principais da aplicacao

--- web-app/src/app/app.routes.ts
+++ web-app/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+import { Dashboard } from './features/ideas/dashboard/dashboard';
+import { Layout } from './shared/components/layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: 'dashboard', component: Dashboard },
+    ]
+  },
+  { path: '**', redirectTo: 'login' }
+];
