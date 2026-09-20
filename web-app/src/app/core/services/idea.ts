b58c9cbb9fd7a0e39e67fe869845343c6b34eb26
Enio Martins <eniomt1.0@gmail.com>
2026-09-20 17:00:47 -0300

    feat(core): adapta servico de ideias para rotas aninhadas de topicos

--- web-app/src/app/core/services/idea.ts
+++ web-app/src/app/core/services/idea.ts
@@ -1,22 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { IdeaResponseDto, IdeaRequestDto } from '../models/api.models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IdeaService {
-  // Aponta para o seu contêiner C# (cis-api) na porta 8005
-  private apiUrl = 'http://localhost:8005/api';
+  private apiUrl = 'http://localhost:8005/api/v1';
 
   constructor(private http: HttpClient) {}
 
-  // Ajuste as rotas '/ideas' ou '/IdeaQuery' conforme os controllers do seu C#
-  getIdeas(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/ideas`);
+  getIdeasByTopic(topicId: string): Observable<IdeaResponseDto[]> {
+    return this.http.get<IdeaResponseDto[]>(`${this.apiUrl}/topics/${topicId}/ideas`);
   }
 
-  vote(ideaId: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/ideas/${ideaId}/vote`, {});
+  createIdea(topicId: string, idea: IdeaRequestDto): Observable<IdeaResponseDto> {
+    return this.http.post<IdeaResponseDto>(`${this.apiUrl}/topics/${topicId}/ideas`, idea);
+  }
+
+  vote(ideaId: string): Observable<IdeaResponseDto> {
+    return this.http.post<IdeaResponseDto>(`${this.apiUrl}/ideas/${ideaId}/vote`, {});
+  }
+
+  unvote(ideaId: string): Observable<IdeaResponseDto> {
+    return this.http.post<IdeaResponseDto>(`${this.apiUrl}/ideas/${ideaId}/unvote`, {});
   }
 }
