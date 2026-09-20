0c3ec260888661d75f0407d5239ef4669b36ebb3
Enio Martins <eniomt1.0@gmail.com>
2026-09-20 17:18:23 -0300

    feat(routing): adiciona rota para a tela de detalhes do topico

--- web-app/src/app/app.routes.ts
+++ web-app/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Login } from './features/auth/login/login';
 import { Register } from './features/auth/register/register';
 import { Dashboard } from './features/ideas/dashboard/dashboard';
 import { Layout } from './shared/components/layout/layout';
+import { IdeaDetail } from './features/ideas/idea-detail/idea-detail'; 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -13,6 +14,8 @@ export const routes: Routes = [
     component: Layout,
     children: [
       { path: 'dashboard', component: Dashboard },
+      // Rota dinâmica que captura o ID do tópico na URL
+      { path: 'topico/:id', component: IdeaDetail } 
     ]
   },
   { path: '**', redirectTo: 'login' }
