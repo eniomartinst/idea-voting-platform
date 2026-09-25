bac242832f81755e0a196c3904ac5618a33e8e9d
Enio Martins <eniomt1.0@gmail.com>
2026-09-25 18:28:22 -0300

    refactor: atualiza namespace para com.ideavoting em UserPersistenceMapperTest.java

--- user-api/src/test/java/jalau/usersapi/infrastructure/mysql/mappers/UserPersistenceMapperTest.java
+++ user-api/src/test/java/jalau/usersapi/infrastructure/mysql/mappers/UserPersistenceMapperTest.java
@@ -1,7 +1,7 @@
-package jalau.usersapi.infrastructure.mysql.mappers;
+package com.ideavoting.usersapi.infrastructure.mysql.mappers;
 
-import jalau.usersapi.core.domain.entities.User;
-import jalau.usersapi.infrastructure.mysql.entities.UserJpaEntity;
+import com.ideavoting.usersapi.core.domain.entities.User;
+import com.ideavoting.usersapi.infrastructure.mysql.entities.UserJpaEntity;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
