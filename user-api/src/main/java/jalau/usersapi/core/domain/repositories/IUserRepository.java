b2b560f10f90c5c7667880cd0258373b9c3f65b9
Enio Martins <eniomt1.0@gmail.com>
2026-09-25 18:28:16 -0300

    refactor: atualiza namespace para com.ideavoting em IUserRepository.java

--- user-api/src/main/java/jalau/usersapi/core/domain/repositories/IUserRepository.java
+++ user-api/src/main/java/jalau/usersapi/core/domain/repositories/IUserRepository.java
@@ -1,6 +1,6 @@
-package jalau.usersapi.core.domain.repositories;
+package com.ideavoting.usersapi.core.domain.repositories;
 
-import jalau.usersapi.core.domain.entities.User;
+import com.ideavoting.usersapi.core.domain.entities.User;
 import java.util.List;
 
 /**
