161b3ebc49bf32d83d2d9a1906acefe32365326c
Enio Martins <eniomt1.0@gmail.com>
2026-09-25 18:28:18 -0300

    refactor: atualiza namespace para com.ideavoting em UserPersistenceMapper.java

--- user-api/src/main/java/jalau/usersapi/infrastructure/mysql/mappers/UserPersistenceMapper.java
+++ user-api/src/main/java/jalau/usersapi/infrastructure/mysql/mappers/UserPersistenceMapper.java
@@ -1,7 +1,7 @@
-package jalau.usersapi.infrastructure.mysql.mappers;
+package com.ideavoting.usersapi.infrastructure.mysql.mappers;
 
-import jalau.usersapi.core.domain.entities.User;
-import jalau.usersapi.infrastructure.mysql.entities.UserJpaEntity;
+import com.ideavoting.usersapi.core.domain.entities.User;
+import com.ideavoting.usersapi.infrastructure.mysql.entities.UserJpaEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
