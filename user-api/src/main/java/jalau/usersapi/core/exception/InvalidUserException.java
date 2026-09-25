83404c1f5b27a8e975fa88956194c77b6eeec27e
Enio Martins <eniomt1.0@gmail.com>
2026-09-25 18:28:17 -0300

    refactor: atualiza namespace para com.ideavoting em InvalidUserException.java

--- user-api/src/main/java/jalau/usersapi/core/exception/InvalidUserException.java
+++ user-api/src/main/java/jalau/usersapi/core/exception/InvalidUserException.java
@@ -1,4 +1,4 @@
-package jalau.usersapi.core.exception;
+package com.ideavoting.usersapi.core.exception;
 
 public class InvalidUserException extends RuntimeException {
     public InvalidUserException(String message) { super(message); }
