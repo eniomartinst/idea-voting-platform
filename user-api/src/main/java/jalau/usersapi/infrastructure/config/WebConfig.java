f02e2eb515a273f3e3a99c12c1926deb5fa84297
Enio Martins <eniomt1.0@gmail.com>
2026-09-25 18:28:18 -0300

    refactor: atualiza namespace para com.ideavoting em WebConfig.java

--- user-api/src/main/java/jalau/usersapi/infrastructure/config/WebConfig.java
+++ user-api/src/main/java/jalau/usersapi/infrastructure/config/WebConfig.java
@@ -1,4 +1,4 @@
-package jalau.usersapi.infrastructure.config;
+package com.ideavoting.usersapi.infrastructure.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
