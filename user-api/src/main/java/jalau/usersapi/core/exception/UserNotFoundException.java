714bc3d11d0ae66cdb57673460e3e4f17f27712a
Enio Martins <eniomt1.0@gmail.com>
2026-09-25 18:28:18 -0300

    refactor: atualiza namespace para com.ideavoting em UserNotFoundException.java

--- user-api/src/main/java/jalau/usersapi/core/exception/UserNotFoundException.java
+++ user-api/src/main/java/jalau/usersapi/core/exception/UserNotFoundException.java
@@ -1,4 +1,4 @@
-package jalau.usersapi.core.exception;
+package com.ideavoting.usersapi.core.exception;
 
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message) {
