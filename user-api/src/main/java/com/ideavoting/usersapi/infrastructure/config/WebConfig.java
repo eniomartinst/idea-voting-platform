60c35286b185094260f41b67df058e6484873140
Enio Martins <eniomt1.0@gmail.com>
2026-09-26 17:42:39 -0300

    refactor(core): migra estrutura de pacotes de jalau para com.ideavoting

--- user-api/src/main/java/com/ideavoting/usersapi/infrastructure/config/WebConfig.java
+++ user-api/src/main/java/com/ideavoting/usersapi/infrastructure/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Libera todos os endpoints da API
-                .allowedOrigins("http://localhost:4200") // Permite o seu Angular
+                .allowedOriginPatterns("http://localhost:4200") // Permite o seu Angular
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
